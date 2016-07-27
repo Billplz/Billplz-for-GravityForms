62dc7576e43846d91e14f7fbc9f24dc28cadedfc
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2016-07-27 22:58:54 +0800

    Update billplz.php

--- billplz-for-gravityforms/billplz.php
+++ billplz-for-gravityforms/billplz.php
@@ -1,16 +1,17 @@
 <?php
 /*
 Plugin Name: Billplz for GravityForms
-Plugin URI: https://www.wanzul-hosting.com
-Description: Integrates Gravity Forms with Billplz, enabling end users to purchase goods and services through Gravity Forms. Please consider a donation to developer. Sumbangan boleh dilakukan disini. <a href="https://www.billplz.com/form/sw2co7ig8" target="_blank">Donate Now</a>
-Version: 3.1
-Author: Wanzul Hosting Enterprise
-Author URI: http://www.wanzul.net
-Text Domain: billplzforgravityformsaddon
+Plugin URI: http://www.facebook.com/billplzplugin
+Description: Integrates Gravity Forms with Billplz Payments, enabling end users to purchase goods and services through Gravity Forms.
+Version: 3.2
+Author: Wan Zulkarnain
+Author URI: http://www.wanzul-hosting.com
+Text Domain: billplzforgravityforms
 Domain Path: /languages
 */
 
-define( 'GF_BILLPLZ_VERSION', '1.2' );
+
+define( 'GF_BILLPLZ_VERSION', '3.2' );
 
 add_action( 'gform_loaded', array( 'GF_Billplz_Bootstrap', 'load' ), 5 );
 
